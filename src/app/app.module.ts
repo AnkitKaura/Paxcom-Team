ed151f5753716e57fb5ba557f159a46511d06f79
ankit.kaura <ankit.kaura@paxcom.net>
2021-03-26 17:46:43 +0530

    removed extra component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ServicesComponent } from './services/services.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ServicesComponent,
-    AboutComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
