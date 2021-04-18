06cfc954dca0adeac79db6027e9b73d902684fbd
ankit.kaura <ankit.kaura@paxcom.net>
2021-04-18 14:06:36 +0530

    basic profile info added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,18 @@ declare var $ : any;
 })
 export class HomeComponent implements OnInit {
   teamArr:any;
+  currentTeamMember: any;
   constructor() { }
 
   ngOnInit(): void {
     this.teamArr = teamlist;
+    this.currentTeamMember = {};
       (<any>$('.js-scroll-trigger')).on('click',(function(){
         (<any>$(".navbar-collapse")).collapse("hide");
       }))
   }
-  
+
+  openOverlay(teamMember:any): void{
+      this.currentTeamMember = teamMember;  
+  }
 }
